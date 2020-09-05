f41eec32a1b066fda25e9d00e75a0bc681dbd04a
ersaab <er.harmanjitsingh@gmail.com>
2020-09-04 23:12:05 -0400

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { QuickQuoteComponent } from './shared/quick-quote/quick-quote.component';
-import { ManuListComponent } from './manufacturer/manu-list/manu-list.component';
-import { CustListComponent } from './customer/cust-list/cust-list.component';
+import { QuickQuoteComponent } from './Shared/quick-quote/quick-quote.component';
+import { ManuListComponent } from './Manufacturer/manu-list/manu-list.component';
+import { CustListComponent } from './Customer/cust-list/cust-list.component';
 
 
 const routes: Routes = [
